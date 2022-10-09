4391b9d828457b70e00a38b45c2e672527c8f8c0
ricardogoncalves-cds <112553821+ricardogoncalves-cds@users.noreply.github.com>
2022-10-09 21:37:15 +0100

    Create binarySearch_V2.cs
    Another implementation of the Binary Search algorithm. This implementation always search the whole array, unlike the version V1 where we can provide a starting and end index to perform the search

---
+++ binarySearch_V2.cs
@@ -0,0 +1,46 @@
+/*
+This is a manual implemenation
+of the Binary search algorithm
+
+It's important to note that the
+array must be sorted in order
+for the Binary search algorithm
+to work
+
+Big O notation: O(log n)
+*/
+
+int[] myArray = {2, 4, 5, 11, 22, 24, 25, 38, 41, 44, 51, 53, 57, 58, 61, 65, 68, 82, 86};
+int target = 82;
+
+if(BinarySearch(myArray, target) != -1)
+{
+    Console.WriteLine($"Item {target} was found in the array");
+}
+else
+{
+    Console.WriteLine($"Item {target} was not found in the array");
+}
+
+int BinarySearch(int[] arr, int target)
+{
+    int left = 0;
+    int right = arr.Length;
+    while(left <= right)
+    {
+        int middle = (left + right) / 2;
+        if(arr[middle] == target)
+        {
+            return target;
+        }
+        else if(target < arr[middle])
+        {
+            right = middle - 1;
+        }
+        else
+        {
+            left = middle + 1;
+        }
+    }
+    return -1;
+}
